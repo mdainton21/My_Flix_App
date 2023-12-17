782fde93d5688e01b48a8e923301c3238cde543d
mdainton21 <142181864+mdainton21@users.noreply.github.com>
2023-12-16 23:03:17 -0500

    Updated CORS
    Updated CORS to allow access on the local host

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.static('public'));
 
 // Cors Setup
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http//localhost:1234'];
 
 app.use(cors({
   origin: (origin, callback) => {
